5c194844610fb92c628393e57914b76cc4a6dea5
MannyMonocle <eetumadu@gmail.com>
2019-11-16 10:31:40 -0500

    Tests added

--- src/test/java/io/zipcoder/polymorphism/MainApplicationTest.java
+++ src/test/java/io/zipcoder/polymorphism/MainApplicationTest.java
@@ -1,7 +1,42 @@
-package io.zipcoder.polymorphism;
+pacpackage io.zipcoder.polymorphism;
+
+import org.junit.Assert;
+import org.junit.Test;
 
 /**
  * Created by leon on 11/6/17.
  */
 public class MainApplicationTest {
+
+    @Test
+    public void petsListSizeTest(){
+        User testUser = new User(3);
+
+        Integer expected = 3;
+        Integer actual = testUser.pets.getsize();
+
+        Assert.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void petsGetNameTest(){
+        Dog snoop = new Dog();
+        snoop.setName("Snoop");
+
+        String expected = "Snoop";
+        String actual = snoop.getName();
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void petsSpeakTest(){
+        Cat burgler = new Cat();
+
+        String expected = "purrr";
+        String actual = burgler.speak();
+
+        Assert.assertEquals(expected, actual);
+    }
 }
